6e0379bae9b8bfcb27b182be7baef982e9008228
Daniela <Daniela@DESKTOP-HT00A5J>
2020-05-29 18:20:35 -0500

    Ejemplo Ciclo For

---
+++ CicloFor/CicloFor/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For a = 1 To Val(TextBox1.Text)
+            Label1.Text = Label1.Text & " " & a
+        Next
+    End Sub
+End Class
