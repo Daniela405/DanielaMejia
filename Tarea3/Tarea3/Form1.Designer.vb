f673765f8440cd393f1c1b1aff946216b65c408b
Daniela <Daniela@DESKTOP-HT00A5J>
2020-05-29 17:39:31 -0500

    Tarea 3

---
+++ Tarea3/Tarea3/Form1.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtISV = New System.Windows.Forms.TextBox()
+        Me.txtDescuento = New System.Windows.Forms.TextBox()
+        Me.txtSubtotal = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.comImpuesto = New System.Windows.Forms.ComboBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.comDescuento = New System.Windows.Forms.ComboBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.Yellow
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.ForeColor = System.Drawing.Color.Black
+        Me.btnSalir.Location = New System.Drawing.Point(410, 173)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(100, 36)
+        Me.btnSalir.TabIndex = 9
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.BackColor = System.Drawing.Color.Yellow
+        Me.btnNuevo.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNuevo.ForeColor = System.Drawing.Color.Black
+        Me.btnNuevo.Location = New System.Drawing.Point(410, 128)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(100, 39)
+        Me.btnNuevo.TabIndex = 8
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = False
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.BackColor = System.Drawing.Color.Yellow
+        Me.btnCalcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalcular.ForeColor = System.Drawing.Color.Black
+        Me.btnCalcular.Location = New System.Drawing.Point(410, 83)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(100, 39)
+        Me.btnCalcular.TabIndex = 7
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.txtTotal)
+        Me.GroupBox2.Controls.Add(Me.txtISV)
+        Me.GroupBox2.Controls.Add(Me.txtDescuento)
+        Me.GroupBox2.Controls.Add(Me.txtSubtotal)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(111, 300)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(252, 225)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Detalle Factura"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(7, 145)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 20)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Total"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(7, 74)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 20)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Descuento"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(7, 110)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 20)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "ISV "
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Enabled = False
+        Me.txtTotal.Location = New System.Drawing.Point(109, 145)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 26)
+        Me.txtTotal.TabIndex = 11
+        '
+        'txtISV
+        '
+        Me.txtISV.Enabled = False
+        Me.txtISV.Location = New System.Drawing.Point(109, 108)
+        Me.txtISV.Name = "txtISV"
+        Me.txtISV.Size = New System.Drawing.Size(100, 26)
+        Me.txtISV.TabIndex = 10
+        '
+        'txtDescuento
+        '
+        Me.txtDescuento.Enabled = False
+        Me.txtDescuento.Location = New System.Drawing.Point(109, 71)
+        Me.txtDescuento.Name = "txtDescuento"
+        Me.txtDescuento.Size = New System.Drawing.Size(100, 26)
+        Me.txtDescuento.TabIndex = 9
+        '
+        'txtSubtotal
+        '
+        Me.txtSubtotal.Enabled = False
+        Me.txtSubtotal.Location = New System.Drawing.Point(109, 36)
+        Me.txtSubtotal.Name = "txtSubtotal"
+        Me.txtSubtotal.Size = New System.Drawing.Size(100, 26)
+        Me.txtSubtotal.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 39)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Subtotal"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.comImpuesto)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.comDescuento)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.txtCantidad)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtPrecio)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(111, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(252, 282)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos Requeridos"
+        '
+        'comImpuesto
+        '
+        Me.comImpuesto.FormattingEnabled = True
+        Me.comImpuesto.Items.AddRange(New Object() {"15%", "17%"})
+        Me.comImpuesto.Location = New System.Drawing.Point(36, 233)
+        Me.comImpuesto.Name = "comImpuesto"
+        Me.comImpuesto.Size = New System.Drawing.Size(154, 28)
+        Me.comImpuesto.TabIndex = 7
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(36, 203)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(154, 24)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "Aplicar Impuesto"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'comDescuento
+        '
+        Me.comDescuento.FormattingEnabled = True
+        Me.comDescuento.Items.AddRange(New Object() {"5%", "10%", "15%", "20%", "30%", "50%"})
+        Me.comDescuento.Location = New System.Drawing.Point(36, 147)
+        Me.comDescuento.Name = "comDescuento"
+        Me.comDescuento.Size = New System.Drawing.Size(154, 28)
+        Me.comDescuento.TabIndex = 5
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(36, 117)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(165, 24)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Aplicar Descuento"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(109, 58)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 26)
+        Me.txtCantidad.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Cantidad"
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(109, 23)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 26)
+        Me.txtPrecio.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "P. Unitario"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(625, 511)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Facturacion"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtISV As TextBox
+    Friend WithEvents txtDescuento As TextBox
+    Friend WithEvents txtSubtotal As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents comImpuesto As ComboBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents comDescuento As ComboBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Tarea3/Tarea3/Form1.vb
@@ -0,0 +1,167 @@
+﻿
+Public Class Form1
+        Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+            txtCantidad.Clear()
+            txtDescuento.Clear()
+            txtISV.Clear()
+            txtPrecio.Clear()
+            txtSubtotal.Clear()
+            txtTotal.Clear()
+            comImpuesto.Text = ""
+            comDescuento.Text = ""
+            CheckBox1.Checked = False
+            CheckBox2.Checked = False
+
+            If btnNuevo.Enabled = True Then
+                btnNuevo.Enabled = False
+            End If
+
+        End Sub
+
+        Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+            End
+        End Sub
+
+        Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+            'declaracion de variables
+            Dim pUnitario, cantidad As Integer
+            Dim descuento, impuesto, subtotal, total As Double
+
+
+            pUnitario = Val(txtPrecio.Text)
+            cantidad = Val(txtCantidad.Text)
+
+
+            descuento = 0
+            subtotal = pUnitario * cantidad
+
+            'condicion para calcular el descuento
+            If comDescuento.Text = "5%" Then
+                descuento = subtotal * 0.05
+                total = subtotal - descuento
+            ElseIf comDescuento.Text = "10%" Then
+                descuento = subtotal * 0.1
+                total = subtotal - descuento
+            ElseIf comDescuento.Text = "15%" Then
+                descuento = subtotal * 0.15
+                total = subtotal - descuento
+            ElseIf comDescuento.Text = "20%" Then
+                descuento = subtotal * 0.2
+                total = subtotal - descuento
+            ElseIf comDescuento.Text = "30%" Then
+                descuento = subtotal * 0.3
+                total = subtotal - descuento
+            ElseIf comDescuento.Text = "50%" Then
+                descuento = subtotal * 0.5
+                total = subtotal - descuento
+            End If
+
+            'condicion para calcular el impuesto
+            If comImpuesto.Text = "15%" Then
+                impuesto = subtotal * 0.15
+                total = subtotal + impuesto
+            ElseIf comImpuesto.Text = "17%" Then
+                impuesto = subtotal * 0.17
+                total = subtotal + impuesto
+
+            End If
+
+
+            txtSubtotal.Text = subtotal
+            txtDescuento.Text = descuento
+            txtISV.Text = impuesto
+            txtTotal.Text = total
+
+
+            'condicion para desabilitar el boton de nuevo 
+            If btnCalcular.Enabled = True Then
+                btnNuevo.Enabled = True
+            Else
+                btnNuevo.Enabled = False
+            End If
+
+
+            'condiciones para validar los digitos ingresados por el usuario
+            If txtPrecio.Text = "" And txtCantidad.Text = "" Then
+                MsgBox("Escriba un numero en la casilla", vbInformation)
+            ElseIf Not IsNumeric(txtPrecio.Text) And Not IsNumeric(txtCantidad.Text) Then
+                MsgBox("Ingrese un valor numerico", vbInformation)
+                txtCantidad.Text = ""
+                txtPrecio.Text = ""
+            ElseIf txtPrecio.Text < 0 Then
+                MsgBox("Número negativo", vbInformation)
+                txtPrecio.Text = ""
+            ElseIf txtCantidad.Text < 0 Then
+                MsgBox("Numero negativo", vbInformation)
+                txtCantidad.Text = ""
+            End If
+
+
+        End Sub
+
+        Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+            If CheckBox1.Checked = False Then
+                comDescuento.Enabled = False
+            ElseIf CheckBox1.Checked = True Then
+                comDescuento.Enabled = True
+
+            End If
+
+            CheckBox2.Checked = False
+        End Sub
+
+        Private Sub comDescuento_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comDescuento.SelectedIndexChanged
+            If CheckBox1.Checked = False Then
+                comDescuento.Enabled = False
+            ElseIf CheckBox1.Checked = True Then
+                comDescuento.Enabled = True
+            End If
+
+            If CheckBox2.Checked = False Then
+                comImpuesto.Enabled = False
+            ElseIf CheckBox2.Checked = True Then
+                comImpuesto.Enabled = True
+
+            End If
+        End Sub
+
+        Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+            CheckBox1.Checked = False
+            If CheckBox2.Checked = False Then
+                comImpuesto.Enabled = False
+            ElseIf CheckBox2.Checked = True Then
+                comImpuesto.Enabled = True
+
+            End If
+
+            If CheckBox1.Checked = False Then
+                comDescuento.Enabled = False
+            ElseIf CheckBox1.Checked = True Then
+                comDescuento.Enabled = True
+
+            End If
+        End Sub
+
+        Private Sub comImpuesto_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comImpuesto.SelectedIndexChanged
+            If CheckBox2.Checked = False Then
+                comImpuesto.Enabled = False
+            ElseIf CheckBox2.Checked = True Then
+                comImpuesto.Enabled = True
+
+            End If
+
+            If CheckBox2.Checked = True Then
+                comImpuesto.Enabled = True
+            ElseIf CheckBox1.Checked = False Then
+                comDescuento.Enabled = False
+
+            End If
+        End Sub
+
+        Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+            btnNuevo.Enabled = False
+            comDescuento.Enabled = False
+            comImpuesto.Enabled = False
+
+        End Sub
+    End Class
